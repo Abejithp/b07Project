6da6860633e609fd45f7aac61abdd90cf1373fd7
Abejithp <abejithp@hotmail.com>
2022-11-30 23:48:53 -0500

    Added the TimelineFragment that allows students to view added courses

--- app/src/main/java/cscb07/group4/androidproject/TimelineFragment.java
+++ app/src/main/java/cscb07/group4/androidproject/TimelineFragment.java
@@ -1,38 +1,26 @@
 package cscb07.group4.androidproject;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import cscb07.group4.androidproject.databinding.FragmentTimelineBinding;
+import cscb07.group4.androidproject.manager.AccountManager;
+import cscb07.group4.androidproject.manager.Course;
+import cscb07.group4.androidproject.manager.CourseManger;
+import cscb07.group4.androidproject.manager.Session;
+import cscb07.group4.androidproject.manager.StudentCourseManager;
 
 public class TimelineFragment extends Fragment {
 
@@ -55,11 +43,11 @@ public class TimelineFragment extends Fragment {
         return binding.getRoot();
     }
     public void update(){
-        if(AccountManager.getInstance().isLoggedIn() && StudentCourseManager.courses!=null){
+        if(AccountManager.getInstance().isLoggedIn()) {
             List<String> courses = StudentCourseManager.getInstance().getWantedCourses();
-            for(String courseCode : courses){
-                if(CourseManger.getInstance().contains(courseCode)){
-                    Course currentCourse = CourseManger.getInstance().getCourseByCode(courseCode);
+            for (String courseCode : courses) {
+                if (CourseManger.getInstance().getCourseByID(courseCode)!=null) {
+                    Course currentCourse = CourseManger.getInstance().getCourseByID(courseCode);
                     TimelineFragment.add(currentCourse);
                 }
             }
@@ -76,11 +64,11 @@ public class TimelineFragment extends Fragment {
 
         if(course!=null&&course.getPrerequisites() != null){
             for(String prerequisite: course.getPrerequisites()){
-                if(StudentCourseManager.getInstance().checkPrerequisite(prerequisite)){
+                if(StudentCourseManager.getInstance().getTakenCourses().contains(prerequisite)){
                     continue;
                 }
 
-                Course current = CourseManger.getInstance().getCourseByCode(prerequisite);
+                Course current = CourseManger.getInstance().getCourseByID(prerequisite);
                 TimelineFragment.add(current);
 
             }
@@ -96,7 +84,8 @@ public class TimelineFragment extends Fragment {
 
     public static void addSession(Course course, TextView textView){
         Session session = course.getSessions().get(0);
-        if(session.compareTo(Session.WINTER)==0){
+
+        if(session == Session.WINTER){
             binding.sessionWinter2023.addView(textView);
         }
         if(session.compareTo(Session.FALL)==0){
@@ -104,8 +93,8 @@ public class TimelineFragment extends Fragment {
         }
         if(session.compareTo(Session.SUMMER)==0){
             binding.sessionSummer2023.addView(textView);
-
         }
+
         return;
     }
 
