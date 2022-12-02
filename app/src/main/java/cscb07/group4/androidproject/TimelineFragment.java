be3a630f0774ab3c43e24034965d5fc9592a8171
CharelSuarez <charel.suarez@mail.utoronto.ca>
2022-12-01 22:18:41 -0500

    Add methods to generate timeline with Abejith.

--- app/src/main/java/cscb07/group4/androidproject/TimelineFragment.java
+++ app/src/main/java/cscb07/group4/androidproject/TimelineFragment.java
@@ -12,8 +12,11 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import cscb07.group4.androidproject.databinding.FragmentTimelineBinding;
 import cscb07.group4.androidproject.manager.AccountManager;
@@ -25,6 +28,8 @@ import cscb07.group4.androidproject.manager.StudentCourseManager;
 public class TimelineFragment extends Fragment {
 
     static FragmentTimelineBinding binding;
+
+    TreeMap<Integer, Set<Course>> timeline = new TreeMap<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -104,4 +109,51 @@ public class TimelineFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private void generateTimeline() {
+        timeline.put(0, new HashSet<>()); // Fall 2022
+        for (String courseCode : StudentCourseManager.getInstance().getWantedCourses()) {
+            Course course = CourseManger.getInstance().getCourseByID(courseCode);
+            if (course != null && !course.getSessions().isEmpty()) {
+                addTimelineCourse(course);
+            }
+        }
+    }
+
+    private int addTimelineCourse(Course course) {
+        int maxSession = -1;
+        if (course.getPrerequisites() != null) {
+            for (String prereqCourseCode : course.getPrerequisites()) {
+                Course prereqCourse = CourseManger.getInstance().getCourseByID(prereqCourseCode);
+                maxSession = Math.max(maxSession, addTimelineCourse(prereqCourse));
+            }
+        }
+
+        if (maxSession == -1) {
+            maxSession = getFirstSession();
+        } else {
+            maxSession++;
+        }
+        // Increment session until we are in an offered session
+        while (!course.getSessions().contains(Session.values()[maxSession % 3])) {
+            maxSession++;
+        }
+        timeline.get(maxSession).add(course);
+        return maxSession;
+    }
+
+    private int getFirstSession() {
+        for (Map.Entry<Integer, Set<Course>> entry : timeline.entrySet()) {
+            // Cannot have more than 6 courses per session
+            if (entry.getValue().size() >= 6) {
+                continue;
+            }
+            return entry.getKey();
+        }
+        // No free session found, add new session
+        int nextSession = timeline.lastKey() + 1;
+        timeline.put(timeline.lastKey() + 1, new HashSet<>());
+        return nextSession;
+    }
+
 }
