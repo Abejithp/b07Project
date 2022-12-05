d9369ae82bd939553c854a0bc08adc4403d50199
priyal1707 <priyalbhasin@gmail.com>
2022-12-04 23:22:06 -0500

    commit 2 added signup and login UI/Back/front .

---
+++ app/src/main/java/cscb07/group4/androidproject/BlankFragment.java
@@ -0,0 +1,83 @@
+package cscb07.group4.androidproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private Button regbutton;
+    private Button logbutton;
+    FragmentActivity context;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment newInstance(String param1, String param2) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        context = getActivity();
+
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+    public void onStart(){
+        super.onStart();
+        Button regbutton = (Button) context.findViewById(R.id.register2_button);
+        regbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, signup.class);
+                startActivity(intent);
+
+            }
+        });
+        Button logbutton = (Button) context.findViewById(R.id.login2_button);
+        logbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
